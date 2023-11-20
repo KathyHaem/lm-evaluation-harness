cfda9f1a1eb202048fa0d2f3469fafda59ddb2a6
Katharina Hämmerl <k.haemmerl@campus.lmu.de>
2023-11-20 14:16:43 +0100

    add minimal implementation of the missing split_and_pad_windows

--- lm_eval/utils.py
+++ lm_eval/utils.py
@@ -179,6 +179,15 @@ def make_disjoint_window(pair):
     return a[: len(a) - (len(b) - 1)], b
 
 
+def split_and_pad_windows(rolling_token_windows, pad_token_id: int, max_seq_len: int):
+    contexts = list(list(zip(*rolling_token_windows))[0])
+    conts = list(list(zip(*rolling_token_windows))[1]) # + [pad_token_id]
+
+    # TODO i think pad to max length in batch -- that said we only ever get one item in here currently
+
+    return contexts, conts
+
+
 def select_continuation_from_batch_left_padding(
     generations: Union[List[List[int]], torch.Tensor], max_context_size: int
 ):
